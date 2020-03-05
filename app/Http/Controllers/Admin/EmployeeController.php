c1ab3973da2fa2ae86100ebd06d5da21b2518f9f
Ominext <thuytheo20101998@gmail.com>
2020-03-05 17:41:44 +0700

    route resource

---
+++ app/Http/Controllers/Admin/EmployeeController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Http\Controllers\Admin;
+
+
+class ThiMa
+{
+    public function index(){
+        return view();
+    }
+    public function show($id){
+        dd($id);
+    }
+}
